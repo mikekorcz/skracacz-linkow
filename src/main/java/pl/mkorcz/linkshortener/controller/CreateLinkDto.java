291b162b9ca687ec9ebc3001e26e156c51d25d18
Michal Korcz <mkorcz01@gmail.com>
2022-10-15 13:57:01 +0200

    added adnotations in / added validator dependency

--- src/main/java/pl/mkorcz/linkshortener/controller/CreateLinkDto.java
+++ src/main/java/pl/mkorcz/linkshortener/controller/CreateLinkDto.java
@@ -3,13 +3,23 @@ package pl.mkorcz.linkshortener.controller;
 import io.swagger.v3.oas.annotations.media.Schema;
 import pl.mkorcz.linkshortener.dto.LinkDto;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Future;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
 record CreateLinkDto(
         @Schema(description = "Identifier/alias to link. Used for redirection", example = "link-alias", required = true)
+        @NotBlank
+        @Size(min = 1, max = 30, message = "Identyfikator musi zawierać od 1 do 30 znaków")
         String id,
+        @NotBlank(message = "Prosze uzupełnić pole")
+        @Email
         String email,
         String targetUrl,
+
+        @Future
         LocalDate expirationDate) {
 
     LinkDto toDto() {
